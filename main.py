b0661c61bdc0a1819293ff354d76fed833f738ed
m7md <m7md.husinn@gmail.com>
2023-10-10 13:34:49 +0300

    The lab is done

---
+++ main.py
@@ -0,0 +1,10 @@
+#Define a variable that has a value of your favourite football team.
+favouriteFootballTeam="AL-Hilal"
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
+x = 50
+y = 30.2525
+#Assign One Value to Multiple Variables (choose random names & values).
+salary1 = salary2 = salary3 = 100
+name1 = name2 = name3 ="Mohammed"
+#Assign multiple Values to Multiple Variables (choose random names & values).
+name,age,city="Mohammed",27,"Abha"
