f130e7cb579ebed6c7001fa97e75f818c4b4cc7f
khamis_dossary <khamis.dossary@gmail.com>
2023-10-10 13:35:12 +0300

    this the solution for the lab

--- LabSolution.py
+++ LabSolution.py
@@ -1,3 +1,4 @@
+'''
 # LAB_VARIABLES
 
 - Define a variable that has a value of your favourite football team.
@@ -5,12 +6,16 @@
 - Assign One Value to Multiple Variables (choose random names & values).
 - Assign multiple Values to Multiple Variables (choose random names & values).
 
+'''
 
+myTeam:str = "Real Madrid"
 
+x:int = 1
 
+y:float = 0.0032
 
+mohammed = issa = mossa = "Prophet"
 
+var1 , var2 , var3 = "this is a string" , 38 , 3.14
 
-
-
-
+print(myTeam, x, y, mohammed,issa,mossa, var1,var2,var3)
