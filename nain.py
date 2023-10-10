5a26066b79067e23cabc79b250766c9290f89d9a
abdullah <abdullah.alanezi.23@gmail.com>
2023-10-10 13:28:36 +0300

    solve lab

---
+++ nain.py
@@ -0,0 +1,15 @@
+#Define a variable that has a value of your favourite football team.
+my_team="AL-Hilal"
+
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
+x=23
+
+y=2.98
+
+#Assign One Value to Multiple Variables (choose random names & values).
+num1 = num2 = num3 = 10
+#Assign multiple Values to Multiple Variables (choose random names & values).
+f_name , s_name, l_name='abdullah' ,"mofarih" ,"alanezi"
+
+
+
