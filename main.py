b54ed3dbcee86a8857ebdf2d9c048a7e5b83273a
AlenaziHazal <roomyham@MacBook-Pro.local>
2023-10-10 13:30:40 +0300

    Lab solution

---
+++ main.py
@@ -0,0 +1,19 @@
+# Lab solve solution.
+
+
+my_favourite_football_teams = "Manchester City"
+print(my_favourite_football_teams)
+#-----
+x = 15
+y = 16.12
+print(x)
+print(y)
+#-----
+
+c = d = s = y
+print(c)
+
+pro,joi,him = "Hero","Man",20
+print(pro)
+print(joi)
+print(him)
