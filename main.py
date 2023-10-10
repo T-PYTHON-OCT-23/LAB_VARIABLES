0e5f3517488e27689756f6fa11a8f2ff2c3e11c0
Anoud <Anoudi90@icloud.com>
2023-10-10 13:32:34 +0300

    solve lab 10 oct

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#solve the solution 
+#Define a variable that has a value of your favourite football team.
+favourateTeam = "Hilal"
+
+#Define a variable “x“ has an integer value and a variable “y” has a float value.
+x =1
+y=True
+
+
+#Assign One Value to Multiple Variables (choose random names & values).
+juice = cheese = cola = 10
+
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+name, age, tall = "sara", 20, 166.5
+
