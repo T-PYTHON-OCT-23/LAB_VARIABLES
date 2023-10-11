88b9b155f2eeb7a8c0e8dd4ff6894c87c202a7a0
hadeel.i7 <hadeel.naji175@gmail.com>
2023-10-11 15:08:23 +0300

    hadeel

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+favouriteTeam = "My favourite football team is AlHilal"
+(print(favouriteTeam))
+
+number1 = float(25/3)
+(print(number1))
+
+number2 = int(25/3)
+(print(number2))
+
+Student1 = Student2 = Student3 = "sara"
+(print(Student2))
+
+Num1 = 40
+Num2 = 25
+Num3 = 90
+(print(Num1+Num2))
