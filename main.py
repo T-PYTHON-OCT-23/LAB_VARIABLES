979cd36c5577ab7bf4a9a570f021469b46fef2eb
Danahalsalmi <danahalmenjaf@gmail.com>
2023-10-20 22:10:16 +0300

    done

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+#- Define a variable that has a value of your favourite football team.
+favourite_team = "alnasser"
+print(favourite_team)
+
+#- Define a variable “x“  has an integer value and a variable “y” has a float value.
+x : int = 55
+y : float = 3.14
+
+print(x)
+print(y)
+
+#- Assign One Value to Multiple Variables (choose random names & values).
+z = d = s = 100
+print(d)
+#- Assign multiple Values to Multiple Variables (choose random names & values).
+num1 , num2 = 50, 80
+print(num1)
+print(num2)
+
+
+
+
+
+
