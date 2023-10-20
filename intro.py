139c1619c5166d3e790cde81ff1e5f6ce4e6f4cf
lama1-abdullah <lama.alothemen21@gmail.com>
2023-10-20 15:17:16 +0300

    Lab var done

---
+++ intro.py
@@ -0,0 +1,20 @@
+# Define a variable that has a value of your favourite football team.
+favourite_football_team ="Al-Hilal Saudi Club"
+print(favourite_football_team)
+
+# Define a variable “x“  has an integer value and a variable “y” has a float value.
+x=22
+y=22.0
+print(x)
+print(y)
+
+# Assign One Value to Multiple Variables (choose random names & values).
+num1 = num2 = num3 = 5
+print(num1)
+
+# Assign multiple Values to Multiple Variables (choose random names & values).
+name, age , weight = "Abdullah", 30 , 170.2
+
+print(name)
+print(age)
+print(weight)
